329c3daab28aa12f0edd5338d0335b2a9ff516d4
leeminwoo <lmw1663@naver.com>
2025-11-26 16:32:43 +0900

    newfile2

--- insecureCode.py
+++ insecureCode.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import requests
 
-# 하드코딩된 비밀번호 (credentials)
+# 하드코딩된 비밀번호 
 DB_PASSWORD = "super_secret_password"  # hardcoded password
 
 def bad_eval(user_input: str):
